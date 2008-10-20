b5480578657d5246eb55d158f6ace625fc3c4b9b
mdemauro <mdemauro@ebc43504-a659-0410-8fb0-e3e426aa280f>
2008-10-20 03:00:06 +0000

    basic test drawing stuff (nothing displaying yet)

--- Assignment3/3DDrawing/3DDrawing.cpp
+++ Assignment3/3DDrawing/3DDrawing.cpp
@@ -14,7 +14,7 @@ void Initialize() {
 	//Init GL
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
-	gluPerspective(45.0, 1.0, 1.0, 10.0);
+	gluPerspective(54.0, 1.0, 1.0, 10.0);
 
 }
 
@@ -23,7 +23,33 @@ void Draw() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	//Draw stuff
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+	gluLookAt( 
+		0.0, 0.0, 2.0, 
+		0.0, 0.0, 0.0, 
+		0.0, 1.0, 0.0 );
+
+	glPushMatrix();	/* save copy of “basic” matrix */
+	//glTranslatef( 1.0, 2.0, 3.0 ); 
+	//glScalef( 2.0, 2.0, 2.0 ); 
+	//glRotatef( 45.0, 0.0, 0.0, 1.0 ); 
+	//glRotatef( 30.0, 0.0, 1.0, 0.0 );
 	
+	
+	/* code to draw object1 */
+	glutWireCube( 1.0 );
+
+
+	glPopMatrix();	/* restore “basic” matrix */
+
+	glPushMatrix();	/* save again */
+	glTranslatef( 10.0, 10.0, 10.0 );
+
+	/* code to draw object2 */
+
+	glPopMatrix();	/* restore again */
+
 
 	glFlush();
 	glutSwapBuffers();

88c3a0dc681b1d920164958c0f92dac401d3c6cf
mdemauro <mdemauro@ebc43504-a659-0410-8fb0-e3e426aa280f>
2008-11-03 00:51:19 +0000

    tesselation - cube done

--- Assignment4/Tesselation/shape.cpp
+++ Assignment4/Tesselation/shape.cpp
@@ -30,6 +30,71 @@ void Shape::addTriangle(const Point3& p1, const Point3& p2, const Point3& p3) {
 Cube::Cube(int n) : Shape() {
 
 	// Your code for tessellating a cube goes here
+	
+	double l = 1.0 / n;
+	double u1, u2, v1, v2;
+	Point3 *p1, *p2, *p3, *p4;
+
+	for(int y = 0; y < n; ++y)  {
+		v1 = l * y - 0.5;
+		v2 = l * (y + 1) - 0.5;
+		for(int x = 0; x < n; ++x) {
+			u1 = l * x - 0.5;
+			u2 = l * (x + 1) - 0.5;
+
+			// -z face
+			p1 = new Point3(u1, v1, -0.5);
+			p2 = new Point3(u2, v1, -0.5);
+			p3 = new Point3(u2, v2, -0.5);
+			p4 = new Point3(u1, v2, -0.5);
+			addTriangle(*p3, *p2, *p1);
+			addTriangle(*p4, *p3, *p1);
+
+			// +z face
+			p1 = new Point3(u1, v1, 0.5);
+			p2 = new Point3(u2, v1, 0.5);
+			p3 = new Point3(u2, v2, 0.5);
+			p4 = new Point3(u1, v2, 0.5);
+			addTriangle(*p1, *p2, *p3);
+			addTriangle(*p1, *p3, *p4);
+
+			// -y face
+			p1 = new Point3(u1, -0.5, v1);
+			p2 = new Point3(u2, -0.5, v1);
+			p3 = new Point3(u2, -0.5, v2);
+			p4 = new Point3(u1, -0.5, v2);
+			addTriangle(*p1, *p2, *p3);
+			addTriangle(*p1, *p3, *p4);
+
+			// +y face
+			p1 = new Point3(u1, 0.5, v1);
+			p2 = new Point3(u2, 0.5, v1);
+			p3 = new Point3(u2, 0.5, v2);
+			p4 = new Point3(u1, 0.5, v2);
+			addTriangle(*p3, *p2, *p1);
+			addTriangle(*p4, *p3, *p1);
+
+			// -x face
+			p1 = new Point3(-0.5, v1, u1);
+			p2 = new Point3(-0.5, v1, u2);
+			p3 = new Point3(-0.5, v2, u2);
+			p4 = new Point3(-0.5, v2, u1);
+			addTriangle(*p1, *p2, *p3);
+			addTriangle(*p1, *p3, *p4);
+
+			// +x face
+			p1 = new Point3(0.5, v1, u1);
+			p2 = new Point3(0.5, v1, u2);
+			p3 = new Point3(0.5, v2, u2);
+			p4 = new Point3(0.5, v2, u1);
+			addTriangle(*p3, *p2, *p1);
+			addTriangle(*p4, *p3, *p1);
+		}
+	}
+	free(p1);
+	free(p2);
+	free(p3);
+	free(p4);
 }
 
 Cone::Cone(int n, int m) : Shape() {
